3a0f2a5f3dc7ed52841e9437f10b446237bffb27
Allan Maia <allanmsantos51@hotmail.com>
2025-04-18 02:24:36 -0300

    Atualizações Links

--- src/components/ListaLinks.tsx
+++ src/components/ListaLinks.tsx
@@ -31,13 +31,16 @@ export default function ListaLinks({ links }: ListaLinksProps) {
     <div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {filteredLinks.map((link, index) => (
-          <div
+          <a
             key={index}
+            href={link.url}  // Usando o href para redirecionar ao clicar
+            target="_blank"
+            rel="noopener noreferrer"
             className="block p-4 bg-gradient-to-br from-yellow-400 via-yellow-500 to-yellow-600 rounded-lg shadow-lg transition-all duration-300 transform hover:scale-105 hover:bg-gradient-to-br from-yellow-500 via-yellow-600 to-yellow-700 hover:shadow-xl"
           >
             <h2 className="text-lg text-white font-semibold mb-2">{link.title}</h2>
             <p className="text-sm text-white truncate">{link.description}</p>
-          </div>
+          </a>
         ))}
       </div>
     </div>
